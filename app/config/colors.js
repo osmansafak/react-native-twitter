303db30e625348d6090f09ef2ba9d89b04190ddf
Osman Safak <osmansafak@gmail.com>
2020-02-17 23:34:32 +0100

    app colors

---
+++ app/config/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  background: '#15202b',
+  tabBackground: '#15202b',
+  text: '#fff',
+  textFaded: '#8899a6',
+  link: '#1da1f2',
+  seperator: '#38444d',
+};
+export default colors;
