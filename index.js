4d49ee2272523bd3732f8d4457be933e02f9ef2b
Osman Safak <osmansafak@gmail.com>
2020-02-17 23:37:23 +0100

    app config

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import * as React from 'react';
+import {AppRegistry, StatusBar, Dimensions} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
 import {name as appName} from './app.json';
+import colors from 'config/colors';
+import Menu from 'navigations/Menu';
+import MainNavigation from 'navigations/Main.js';
 
-AppRegistry.registerComponent(appName, () => App);
+StatusBar.setBarStyle('light-content', true);
+StatusBar.backgroundColor = colors.background;
+
+const Navigation = createDrawerNavigator(
+  {
+    MainNavigation,
+  },
+  {
+    contentComponent: Menu,
+    drawerWidth: Dimensions.get('window').width - 100,
+    drawerType: 'slide',
+  },
+);
+
+const Main = createAppContainer(Navigation);
+const Root = () => <Main />;
+
+AppRegistry.registerComponent(appName, () => Root);
